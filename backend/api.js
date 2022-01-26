30db28080db6c4d55db39bfcf9899677352ca5fc
nar2p1nk <farrisisqandar@gmail.com>
2022-01-26 21:59:44 +0800

    testing POST request with express

--- backend/api.js
+++ backend/api.js
@@ -3,8 +3,12 @@ const db = require('./database');
 const port = 8080;
 const app = express();
 const cors = require('cors');
+const bodyParser = require('bodyParser')
 
 app.use(cors())
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.json({'message':"ass"})
